739802d2a032e90ee0a330e0d93d5a81062cd665
Alesia Rebkavets <rakalesyanyan@gmail.com>
2022-11-19 03:12:56 +0300

    Initial commit

---
+++ Hometask_Lec7/Task3/MobilePhone.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_Lec7
+    {
+        public class MobilePhone
+        {
+            private double numberOfPixelsInCamera;
+            private string? modelName;
+            private decimal price;
+
+            public MobilePhone(double numberOfPixelsInCamera, string? modelName, decimal price)
+            {
+                this.numberOfPixelsInCamera = numberOfPixelsInCamera;
+                this.modelName = modelName;
+                this.price = price;
+            }
+
+            public string Description
+            {
+                get
+                {
+                    return $"Price: {price}, model:{modelName}, number of pixels in camera: {numberOfPixelsInCamera}";
+                }
+            }
+
+            public void TakePhoto()
+            {
+                Console.WriteLine("Press button on the screen and photo is ready");
+            }
+
+            public void TurnOn()
+            {
+                Console.WriteLine("Press left side button");
+            }
+
+            public void TunrnOff()
+            {
+                Console.WriteLine("Press Turn Off button");
+            }
+        }
+    }
---
+++ Hometask_Lec7/Task3/Printer.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+
+namespace Hometask_Lec7
+{
+    public class Printer
+    {
+        private int paperWidth;
+        private int paperHeight;
+        private string? modelName;
+        private decimal price;
+
+        public Printer(string? modelName, decimal price, int paperWidth, int paperHeight)
+        {
+            this.modelName = modelName;
+            this.price = price;
+            this.paperWidth = paperWidth;
+            this.paperHeight = paperHeight;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return $"Price: {price}, model:{modelName}";
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Press button at the top");
+        }
+
+        public void TunrnOff()
+        {
+            Console.WriteLine("Press Turn Off button");
+        }
+    }
+}
